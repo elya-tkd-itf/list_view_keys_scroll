79dfc5ca8b07066313730b63a0a0a9d95a144a00
elya.tkd.itf <elya.tkd.itf@gmail.com>
2021-07-12 22:03:24 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Map<String, dynamic> data = {
+    'Мои фото': [
+      'https://picsum.photos/1200/501',
+      'https://picsum.photos/1200/502',
+      'https://picsum.photos/1200/503',
+      'https://picsum.photos/1200/504',
+      'https://picsum.photos/1200/505',
+      'https://picsum.photos/1200/506',
+      'https://picsum.photos/1200/507',
+      'https://picsum.photos/1200/508',
+      'https://picsum.photos/1200/509',
+      'https://picsum.photos/1200/510',
+    ],
+    'Галерея': [
+      'https://picsum.photos/1200/511',
+      'https://picsum.photos/1200/512',
+      'https://picsum.photos/1200/513',
+      'https://picsum.photos/1200/514',
+      'https://picsum.photos/1200/515',
+      'https://picsum.photos/1200/516',
+      'https://picsum.photos/1200/517',
+      'https://picsum.photos/1200/518',
+      'https://picsum.photos/1200/519',
+      'https://picsum.photos/1200/520',
+    ]
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: data.keys.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Homework example'),
+          bottom: TabBar(
+            tabs: data.keys.map((e) => Tab(text: e.toString())).toList()
+          )
+        ),
+        body: TabBarView(
+          children: data.keys.map((name) {
+            return ListView(
+              key: PageStorageKey(name),
+              children: <Widget>[
+                ...data[name].map((e) {
+                  return Image.network(e);
+                }).toList()
+              ],
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
